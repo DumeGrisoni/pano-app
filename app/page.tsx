068fb611c727d4266cc0f9d560b75151d41e99a5
Dominique Grisoni <misanthrope20218@gmail.com>
2026-03-22 18:38:20 +0100

    add desktop Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Image from 'next/image';
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full flex-col items-center justify-between p-16 bg-white dark:bg-black sm:items-start">
-        <Image src={'/logo.svg'} alt="logo" width={150} height={150} />
-      </main>
+      <main className="flex min-h-screen flex-1 w-full flex-col items-center justify-between p-16 bg-white dark:bg-black sm:items-start"></main>
     </div>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,64 @@
+'use client';
 import React from 'react';
+import { useMediaQuery } from '@/hooks/use-media-query';
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import { MenuIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const isDesktop = useMediaQuery('(min-width: 768px)');
+
+  const pathname = usePathname();
+
+  const navLinks = [
+    { href: '/', label: 'ACCUEIL' },
+    { href: '/products', label: 'PRODUITS' },
+    { href: '/clients', label: 'CLIENTS' },
+    { href: '/projects', label: 'PROJETS' },
+    { href: '/commandes', label: 'COMMANDES' },
+  ];
+
+  return isDesktop ? (
+    <div className="flex fixed w-full justify-between items-center px-5 py-3 bg-white text-black border-b border-b-gray-200 shadow-md shadow-gray/20">
+      <Image src="/logo.svg" alt="Logo" width={50} height={50} />
+      <div className="flex justify-center gap-4 p-4">
+        {navLinks.map((link) => {
+          const isActive = pathname === link.href;
+
+          return (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={`relative hover:underline hover:underline-offset-4 transition-all duration-300 ease-in-out hover:text-red-500 ${
+                isActive ? 'underline underline-offset-4' : ''
+              }`}
+            >
+              {link.label}
+            </Link>
+          );
+        })}
+      </div>
+      <Link href="/login" className="">
+        <Button size={'lg'} className={'cursor-pointer'}>
+          Connexion
+        </Button>
+      </Link>
+    </div>
+  ) : (
+    <div>Mobile</div>
+  );
 };
 
 export default Navbar;
